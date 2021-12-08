efb6a6188d558e1411f414b83deb25554d197585
Gyan Prakash <Gyan.Prakash1@gds.ey.com>
2021-12-08 13:08:50 +0530

    First commit

---
+++ src/spotify.js
@@ -0,0 +1,27 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "https://spotifyclone-3417a.web.app/";
+const clientId = "f026ffa2cf4649b9a649b451f631f0a6";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const getTokenFromUrl = () => {
+    return window.location.hash
+        .substring(1)
+        .split("&")
+        .reduce((initial, item) => {
+            let parts = item.split("=");
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+            return initial;
+        }, {});
+};
+
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    "%20"
+)}&response_type=token&show_dialog=true`;
